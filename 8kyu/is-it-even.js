56485f631dfa9dd6c2c275321f48085d025023e3
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-03-31 21:29:23 -0400

    added 8kyu challenge

---
+++ 8kyu/is-it-even.js
@@ -0,0 +1,23 @@
+/*
+Description:
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+function testEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(testEven(4)); // Outputs: true
+console.log(testEven(5)); // Outputs: false
