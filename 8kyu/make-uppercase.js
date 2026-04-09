2dedc4f7e5a9e71029e4aa15f1af128ea2457053
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-04-08 21:27:41 -0400

    added 8kyu challege

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,10 @@
+/*
+Description:
+Write a function which converts the input string to uppercase.
+*/
+
+function makeUpperCase(str) {
+  let greeting = str.toUpperCase();
+  return greeting;
+}
+console.log(makeUpperCase("hello world!")); //Output: HELLO WORLD!
