5ddeade5f35379d003e09e21f175478e2eaa2860
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-05-28 21:35:32 -0400

    added 7kyu challenge

---
+++ 7kyu/sort-array-by-string-length.js
@@ -0,0 +1,10 @@
+/*
+Description:
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+*/
+
+function sortByLength(array) {
+  return array.sort((a, b) => a.length - b.length);
+}
+
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"])); // Output: [ 'Eyes', 'Glasses', 'Monocles', 'Telescopes' ]
