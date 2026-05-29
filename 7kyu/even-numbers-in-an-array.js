b02078056d6cd2d5b4220e859cadd4e295942275
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-05-29 13:30:59 -0400

    added 7kyu challenge

---
+++ 7kyu/even-numbers-in-an-array.js
@@ -0,0 +1,18 @@
+/*
+Description:
+Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+*/
+
+function evenNumbers(array, number) {
+  let evens = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      evens.push(array[i]);
+    }
+  }
+  return evens.slice(-number);
+}
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // Output: [4, 6, 8]
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)); // Output: [-8, 26]
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1)); // Output: [6]
