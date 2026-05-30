b9b85cada83946532afe08639dc60404e84dc178
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-05-30 13:57:47 -0400

    added 7kyu challenge

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,9 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+*/
+function solution(str, ending) {
+  return str.endsWith(ending) ? true : false;
+}
+
+console.log(solution("abc", "bc")); // Output: true
+console.log(solution("abc", "d")); // Output: false
