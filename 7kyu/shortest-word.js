9f3647080175b3b42406b555e2f53f3f8c729f23
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-06-02 21:03:37 -0400

    added 7kyu challenge

---
+++ 7kyu/shortest-word.js
@@ -0,0 +1,20 @@
+/*
+Description:
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+  let words = s.split(" ");
+  let shortest = words[0];
+
+  for (let word of words) {
+    if (word.length < shortest.length) {
+      shortest = word;
+    }
+  }
+  return shortest.length;
+}
+
+console.log(findShort("this is a string")); // Output: 1
