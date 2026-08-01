6f82552de30748668d013b13376f2ddc8cd7bf29
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-07-31 21:19:18 -0400

    added 8kyu challenge

---
+++ 8kyu/square-sum.js
@@ -0,0 +1,15 @@
+/*
+Description:
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+*/
+
+function squareSum(numbers) {
+  let result = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let square = numbers[i] ** 2;
+    result += square;
+  }
+  return result;
+}
+console.log(squareSum([1, 2, 2])); // Output: 9
