929f023b492b1aed9d23e9dc6e2f0dc6c95e624b
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-08-08 13:42:36 -0400

    added 7kyu challenge

---
+++ 7kyu/validate-a-pin-code.js
@@ -0,0 +1,25 @@
+/*
+Description:
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+*/
+
+function validatePIN(pin) {
+  if (pin.length !== 4 && pin.length !== 6) {
+    return false;
+  }
+
+  for (let i = 0; i < pin.length; i++) {
+    if (pin[i] < "0" || pin[i] > "9") {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(validatePIN("1234")); // Output: true
+console.log(validatePIN("12345")); // Output: false
+console.log(validatePIN("-1234")); // Output: false
+console.log(validatePIN("00000000")); // Output: false
+console.log(validatePIN("-1.234")); // Output: false
+console.log(validatePIN("86.1")); // Output: false
